2c7ecd7425e46fdb2272b7b1cc2955fba21f4906
mual-de <dev@alexandermaxmueller.de>
2022-11-20 15:42:22 +0100

    Added Tests for YAML Point integration

--- test/Point/Point_test.cpp
+++ test/Point/Point_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "Point.hpp"
+#include <yaml-cpp/yaml.h>
 
 using namespace FIPP;
 // Demonstrate some basic assertions.
@@ -51,6 +52,25 @@ TEST(ValueTest, Float)
   EXPECT_EQ(pInt.getYInt(), 20);
   EXPECT_EQ(pInt.getXFloat(), 10.00f);
   EXPECT_EQ(pInt.getYFloat(), 20.00f);
+}
+
+TEST(YamlTest, Get){
+  YAML::Node testNode = Point<int>(10, -20).asYaml();
+  EXPECT_EQ(testNode["x"].as<int>(), 10);
+  EXPECT_EQ(testNode["y"].as<int>(), -20);
+
+  YAML::Node testNodeFloat = Point<float>(10.00f, -20.00f).asYaml();
+  EXPECT_EQ(testNodeFloat["x"].as<int>(), 10.00f);
+  EXPECT_EQ(testNodeFloat["y"].as<int>(), -20.00f);
+}
+
+TEST(YamlTest, Set){
+  YAML::Node testNode;
+  testNode["x"] = 10;
+  testNode["y"] = -20;
+  Point<int> test = PointFactory::ptIntFromYAML(testNode);
+  EXPECT_EQ(test.getX(), 10);
+  EXPECT_EQ(test.getY(), -20);
 }
 
 int main(int argc, char **argv)
